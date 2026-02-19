fb8e8c2e59091b9ac7a6c1706354e94113da2110
Rishon <mithicalaviator85@gmail.com>
2026-02-19 15:54:28 +0530

    Finished shellbridge library

---
+++ src/main/java/iso/MDEngine.java
@@ -0,0 +1,9 @@
+package iso.shellbridge;
+
+import java.io.File;
+
+public class MDEngine {
+    public String[] readMeta(){
+        File file = new File();
+    }
+}
---
+++ src/main/java/iso/shellbridge/CommandList.java
@@ -0,0 +1,4 @@
+package iso.shellbridge;
+
+public class CommandList {
+}
